00a6a0e757467a6603e9529eba3bb7366242a497
Janos Poor <poorjanos@gmail.com>
2019-02-19 12:04:37 +0100

    Add data_export.R

---
+++ R/data_export.R
@@ -0,0 +1,19 @@
+# Check names in original
+names(t_ccc_pa)
+
+
+# Concat variables
+t_ccc_pa_PG_export <- t_ccc_pa %>%
+  mutate(ACTIVITY_COMB_EN = paste(EVENT_CHANNEL, ACTIVITY_EN)) %>%
+  select(CASE_ID, EVENT_END, ACTIVITY_COMB_EN, CASE_TYPE_EN, CASE_TYPE_PROB_CAT, USER_ID) %>%
+  arrange(CASE_ID, EVENT_END)
+
+
+# Change to names to fit PG
+names(t_ccc_pa_PG_export) <- c("Case ID", "Event end", "Activity", "Case type", "Case owner", "User")
+
+
+# Save
+write.table(t_ccc_pa_PG_export,
+            here::here("Data", "t_ccc_pa_PG_export.csv"),
+            row.names = FALSE, sep = ";", quote = FALSE)
