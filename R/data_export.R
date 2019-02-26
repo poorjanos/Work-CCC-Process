bfacde97581a3ef5b75beb2167902c90d6f1a547
Janos Poor <poorjanos@gmail.com>
2019-02-26 13:38:29 +0100

    Add simplified activity to data extraction

--- R/data_export.R
+++ R/data_export.R
@@ -1,7 +1,9 @@
+library(dplyr)
+
 # Check names in original
 names(t_ccc_pa)
 
-
+# Concat media and activity -------------------------------------------------------------
 # Concat variables
 t_ccc_pa_PG_export <- t_ccc_pa %>%
   mutate(ACTIVITY_COMB_EN = paste(EVENT_CHANNEL, ACTIVITY_EN)) %>%
@@ -16,4 +18,23 @@ names(t_ccc_pa_PG_export) <- c("Case ID", "Event end", "Activity", "Case type", 
 # Save
 write.table(t_ccc_pa_PG_export,
             here::here("Data", "t_ccc_pa_PG_export.csv"),
+            row.names = FALSE, sep = ";", quote = FALSE)
+
+
+
+
+# Separate media and activity -------------------------------------------------------------
+t_ccc_pa_PG_export <- t_ccc_pa %>%
+  select(CASE_ID, EVENT_END, ACTIVITY_EN, EVENT_CHANNEL,
+         CASE_TYPE_EN, CASE_TYPE_PROB_CAT, USER_ID) %>%
+  arrange(CASE_ID, EVENT_END)
+
+
+# Change to names to fit PG
+names(t_ccc_pa_PG_export) <- c("Case ID", "Event end", "Activity", "Supplier", "Case type", "Case owner", "User")
+
+
+# Save
+write.table(t_ccc_pa_PG_export,
+            here::here("Data", "t_ccc_pa_PG_export_sepmedia.csv"),
             row.names = FALSE, sep = ";", quote = FALSE)
