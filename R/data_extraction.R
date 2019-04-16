db550af41ed29db4be2daa8d155647fd58a6b774
Janos Poor <poorjanos@gmail.com>
2019-04-16 08:56:08 +0200

    Update data extract for 3 major request types

--- R/data_extraction.R
+++ R/data_extraction.R
@@ -1,5 +1,7 @@
 library(config)
 library(here)
+library(dplyr)
+library(tidyr)
 
 
 #########################################################################################
@@ -36,17 +38,41 @@ jdbcConnection <-
   )
 
 # Fetch data
-query_ccc_process <- "select distinct * from T_CCC_PA_OUTPUT4"
+query_ccc_process <- "select distinct * from T_CCC_PA_OUTPUT_TOP3"
 t_ccc_pa_raw <- dbGetQuery(jdbcConnection, query_ccc_process)
 
 # Close db connection: kontakt
 dbDisconnect(jdbcConnection)
 
 
-# Write to csv
+# Write raw data to csv
 write.table(t_ccc_pa_raw, here::here("Data" ,"t_ccc_pa_raw_2.csv"), row.names = FALSE, sep = ";", quote = FALSE)
 
 t_ccc_pa <- read.csv(here::here("Data" ,"t_ccc_pa_raw_2.csv"), sep = ";", stringsAsFactors = FALSE)
+
+
+# Concat media and activity -------------------------------------------------------------
+# Concat variables
+t_ccc_pa_PG_export <- t_ccc_pa_raw %>%
+  mutate(ACTIVITY_COMB_EN = paste(EVENT_CHANNEL, ACTIVITY_EN)) %>%
+  replace_na(list(PRODUCT_LINE = "UNKNOWN", PRODUCT_CODE = "UNKNOWN")) %>%
+  select(CASE_ID, EVENT_END, ACTIVITY_COMB_EN, ACTIVITY_EN, EVENT_CHANNEL,
+         CASE_TYPE_EN, CASE_TYPE_PROB_CAT, USER_ID, PRODUCT_CODE, PRODUCT_LINE) %>%
+  arrange(CASE_ID, EVENT_END)
+
+
+# Change to names to fit PG
+names(t_ccc_pa_PG_export) <- c("Case ID", "Event end", "Activity", "Activity separate", "Channel", "Case type", "Case owner", "User", "Customer", "Customer type")
+
+
+# Write PG ready data to csv
+write.table(t_ccc_pa_PG_export,
+            here::here("Data", "t_ccc_pa_PG_export_top3.csv"),
+            row.names = FALSE, sep = ";", quote = FALSE)
+
+
+
+
 
 
    
--- SQL/preprocess_event_log_3major_request_types.sql
+++ SQL/preprocess_event_log_3major_request_types.sql
@@ -1,11 +1,11 @@
-/* Formatted on 2019. 02. 15. 13:00:37 (QP5 v5.115.810.9015) */
-/* Preprocess CCC logdata */
+/* Preprocess CCC logdata for 3 major request types*/
 
-/* Gen base table and rename cols */
-DROP TABLE T_CCC_PA_OUTPUT5;
+/*******************************************************************************/
+/* Gen base table and rename cols for CONTRACT DELETION */
+DROP TABLE T_CCC_PA_OUTPUT_TORLES;
 COMMIT;
 
-CREATE TABLE T_CCC_PA_OUTPUT5
+CREATE TABLE T_CCC_PA_OUTPUT_TORLES
 AS
    SELECT   f_paid AS case_id,
             f_idopont AS event_end,
@@ -44,26 +44,217 @@ AS
                AS case_type_prob_cat,
             product_line,
             product_code,
-            attrib1 as info_reason_hu,
-            case when attrib1 = 'AEGON elerhetosegek kiadasa' then 'Provide AEGON contact info'
-            when attrib1 = 'Ajanlat allapot informacio keres' then 'Provide proposal status info'
-            when  attrib1 = 'Dijrendezettseg' then 'Premium status'
-            when attrib1 = 'Intervencios visszajelzes' then 'Intervention feedback'
-            when attrib1 = 'Modozati informacioadas' then 'Provide product info'
-            else 'Other'
-            as info_reason_en
+            attrib1,
+            attrib1eng
      FROM   mesterr.export_pa_wflog3
     WHERE       wflog_user LIKE 'CCC/%'
             AND f_paid LIKE 'S%'
             AND hun1 IS NOT NULL
             AND TRIM (hun1) <> 'Visszalepes'
-            and attrib0 in ('Torles', 'Tajekoztatas');
+            and attrib0 = 'Torles';
 
 COMMIT;
 
 
+/* Compute most common info types to upgrade activity names*/
+DROP TABLE t_ccc_info_reason_most_common_torles;
+COMMIT;
+
+CREATE TABLE t_ccc_info_reason_most_common_torles
+as
+SELECT   attrib1, attrib1eng
+  FROM   (SELECT   attrib1,
+                   attrib1eng,
+                   db,
+                   db / SUM (db) OVER () AS ratio
+            FROM   (  SELECT   attrib1, attrib1eng, COUNT (case_id) AS db
+                        FROM   T_CCC_PA_OUTPUT_TORLES
+                       WHERE   activity_hu = 'Tajekoztatas'
+                    GROUP BY   attrib1, attrib1eng
+                    ORDER BY   3 DESC))
+ WHERE   ratio > 0.01;
+
+
+UPDATE T_CCC_PA_OUTPUT_TORLES
+set attrib1 = 'Egyéb' where attrib1 not in (select attrib1 from t_ccc_info_reason_most_common_torles);
+
+UPDATE T_CCC_PA_OUTPUT_TORLES
+set attrib1eng = 'Other' where attrib1eng not in (select attrib1eng from t_ccc_info_reason_most_common_torles);
+
+
+UPDATE T_CCC_PA_OUTPUT_TORLES
+set activity_hu = activity_hu || ' ' || attrib1;
+
+
+UPDATE T_CCC_PA_OUTPUT_TORLES
+set activity_en = activity_en || ' ' || attrib1eng;
+
+COMMIT;
+
+/*******************************************************************************/
+/* Gen base table and rename cols for PROVIDE INFORMATION */
+DROP TABLE T_CCC_PA_OUTPUT_TAJ;
+COMMIT;
+
+CREATE TABLE T_CCC_PA_OUTPUT_TAJ
+AS
+   SELECT   f_paid AS case_id,
+            f_idopont AS event_end,
+            wflog_user AS user_id,
+            CASE
+               WHEN attrib2 = 'Call Center' THEN 'CALL'
+               WHEN attrib2 = 'Mail' THEN 'MAIL'
+               WHEN attrib2 = 'Fax' THEN 'FAX'
+               WHEN attrib2 = 'PubWeb' THEN 'PWEB'
+               ELSE 'DOC'
+            END
+               AS event_channel,
+                        CASE
+               WHEN hun1 = 'Vegrehajtva'
+               THEN
+                  'Vegrehajtas'
+               WHEN hun1 = 'Reklamacio  OKK-ba tovabbitva'
+               THEN
+                  'Reklamacio OKK-ba tovabbitas'
+               ELSE
+                  REGEXP_REPLACE (hun1, '->.*', '')
+            END
+               AS activity_hu,
+            REGEXP_REPLACE (hun1eng, '->.*', '') AS activity_en,
+            attrib0 AS case_type_hu,
+            attrib0eng AS case_type_en,
+            attrib0_p AS case_type_prob,
+            CASE
+               WHEN attrib0_p >= 0.9 THEN '0.9-'
+               WHEN attrib0_p BETWEEN 0.8 AND 0.89999 THEN '0.8-0.9'
+               WHEN attrib0_p BETWEEN 0.7 AND 0.79999 THEN '0.7-0.8'
+               WHEN attrib0_p BETWEEN 0.6 AND 0.69999 THEN '0.6-0.7'
+               WHEN attrib0_p BETWEEN 0.5 AND 0.59999 THEN '0.5-0.6'
+               WHEN attrib0_p < 0.5 THEN '-0.5'
+            END
+               AS case_type_prob_cat,
+            product_line,
+            product_code,
+            attrib1,
+            attrib1eng
+     FROM   mesterr.export_pa_wflog3
+    WHERE       wflog_user LIKE 'CCC/%'
+            AND f_paid LIKE 'S%'
+            AND hun1 IS NOT NULL
+            AND TRIM (hun1) <> 'Visszalepes'
+            and attrib0 = 'Tajekoztatas';
+
+COMMIT;
+
+/* Compute most common info types to upgrade activity names*/
+DROP TABLE t_ccc_info_reason_most_common_taj;
+COMMIT;
+
+CREATE TABLE t_ccc_info_reason_most_common_taj
+as
+SELECT   attrib1, attrib1eng
+  FROM   (SELECT   attrib1,
+                   attrib1eng,
+                   db,
+                   db / SUM (db) OVER () AS ratio
+            FROM   (  SELECT   attrib1, attrib1eng, COUNT (case_id) AS db
+                        FROM   T_CCC_PA_OUTPUT_TAJ
+                       WHERE   activity_hu = 'Tajekoztatas'
+                    GROUP BY   attrib1, attrib1eng
+                    ORDER BY   3 DESC))
+ WHERE   ratio > 0.01;
+
+
+UPDATE T_CCC_PA_OUTPUT_TAJ
+set attrib1 = 'Egyéb' where attrib1 not in (select attrib1 from t_ccc_info_reason_most_common_taj);
+
+UPDATE T_CCC_PA_OUTPUT_TAJ
+set attrib1eng = 'Other' where attrib1eng not in (select attrib1eng from t_ccc_info_reason_most_common_taj);
+
+
+UPDATE T_CCC_PA_OUTPUT_TAJ
+set activity_hu = activity_hu || ' ' || attrib1;
+
+
+UPDATE T_CCC_PA_OUTPUT_TAJ
+set activity_en = activity_en || ' ' || attrib1eng;
+
+COMMIT;
+
+
+/*******************************************************************************/
+/* Gen base table and rename cols for CLAIM REPORT */
+DROP TABLE T_CCC_PA_OUTPUT_KAR;
+COMMIT;
+
+CREATE TABLE T_CCC_PA_OUTPUT_KAR
+AS
+   SELECT   f_paid AS case_id,
+            f_idopont AS event_end,
+            wflog_user AS user_id,
+            CASE
+               WHEN attrib2 = 'Call Center' THEN 'CALL'
+               WHEN attrib2 = 'Mail' THEN 'MAIL'
+               WHEN attrib2 = 'Fax' THEN 'FAX'
+               WHEN attrib2 = 'PubWeb' THEN 'PWEB'
+               ELSE 'DOC'
+            END
+               AS event_channel,
+                        CASE
+               WHEN hun1 = 'Vegrehajtva'
+               THEN
+                  'Vegrehajtas'
+               WHEN hun1 = 'Reklamacio  OKK-ba tovabbitva'
+               THEN
+                  'Reklamacio OKK-ba tovabbitas'
+               ELSE
+                  REGEXP_REPLACE (hun1, '->.*', '')
+            END
+               AS activity_hu,
+            REGEXP_REPLACE (hun1eng, '->.*', '') AS activity_en,
+            attrib0 AS case_type_hu,
+            attrib0eng AS case_type_en,
+            attrib0_p AS case_type_prob,
+            CASE
+               WHEN attrib0_p >= 0.9 THEN '0.9-'
+               WHEN attrib0_p BETWEEN 0.8 AND 0.89999 THEN '0.8-0.9'
+               WHEN attrib0_p BETWEEN 0.7 AND 0.79999 THEN '0.7-0.8'
+               WHEN attrib0_p BETWEEN 0.6 AND 0.69999 THEN '0.6-0.7'
+               WHEN attrib0_p BETWEEN 0.5 AND 0.59999 THEN '0.5-0.6'
+               WHEN attrib0_p < 0.5 THEN '-0.5'
+            END
+               AS case_type_prob_cat,
+            product_line,
+            product_code,
+            attrib1,
+            attrib1eng
+     FROM   mesterr.export_pa_wflog3
+    WHERE       wflog_user LIKE 'CCC/%'
+            AND f_paid LIKE 'K%'
+            AND hun1 IS NOT NULL
+            AND TRIM (hun1) <> 'Visszalepes'
+            and attrib0 = 'Karbejelentes';
+
+COMMIT;
+
+
+/*******************************************************************************/
+/* Merge top3 */
+DROP TABLE T_CCC_PA_OUTPUT_TOP3;
+COMMIT;
+
+CREATE TABLE T_CCC_PA_OUTPUT_TOP3
+as 
+select * from T_CCC_PA_OUTPUT_TORLES
+UNION
+select * from T_CCC_PA_OUTPUT_TAJ
+UNION
+select * from T_CCC_PA_OUTPUT_KAR;
+COMMIT;
+
+
 /* Define then drop cases with first event outside 201701.01. and 2018.12.01.*/
-DELETE FROM   T_CCC_PA_OUTPUT5
+DELETE FROM   T_CCC_PA_OUTPUT_TOP3
       WHERE   case_id IN
                     (SELECT   case_id
                        FROM   (  SELECT   case_id,
